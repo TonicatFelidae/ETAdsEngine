073b45bdd428df5c0a7371ae9045b177757f63fb
TonicatFelidae <50703992+TonicatFelidae@users.noreply.github.com>
2026-02-05 15:48:13 +0700

    update ads setup

--- AdsSetup.ts
+++ AdsSetup.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, sys } from 'cc';
 import { super_html_playable } from './SuperHtml/super_html_playable';
 const { ccclass, property } = _decorator;
 const playableAds = new super_html_playable();
@@ -9,6 +9,11 @@ export class AdsSetup extends Component {
     start() {
        playableAds.set_google_play_url(this.gameUrl);
    }
+    public touchDownloadButton()
+    {
+        sys.openURL(this.gameUrl);
+        playableAds.download();
+    }
 }
 
 
