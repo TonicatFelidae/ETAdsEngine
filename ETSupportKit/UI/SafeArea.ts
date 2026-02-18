ab7d3bebbee2c4c24a75903b3204e90172245571
Tania Felidea <50703992+TonicatFelidae@users.noreply.github.com>
2026-02-19 05:13:22 +0700

    Add safe area

---
+++ ETSupportKit/UI/SafeArea.ts
@@ -0,0 +1,32 @@
+import { _decorator, Component, view, sys, Widget } from 'cc';
+const { ccclass } = _decorator;
+
+@ccclass('SafeArea')
+export class SafeArea extends Component {
+
+    start () {
+
+        if (!sys.isMobile || sys.os !== sys.OS.IOS) return;
+
+        const safe = sys.getSafeAreaRect();
+        const visible = view.getVisibleSize();
+        const design = view.getDesignResolutionSize();
+
+        const scaleX = design.width / visible.width;
+        const scaleY = design.height / visible.height;
+
+        const top = (visible.height - (safe.y + safe.height * 2)) * scaleY;
+        const bottom = safe.y * scaleY;
+        const left = safe.x * scaleX;
+        const right = (visible.width - (safe.x + safe.width)) * scaleX;
+
+        const widget = this.getComponent(Widget)!;
+
+        widget.top = top;
+        widget.bottom = bottom;
+        widget.left = left;
+        widget.right = right;
+
+        widget.updateAlignment();
+    }
+}
